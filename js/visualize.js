fbd1f1fd7dc5d97b8f6bde3b7c3ff6af0bb659cc
Lester Lee <ll6@williams.edu>
2018-11-26 17:39:41 -0500

    Nodes are showing up

--- js/visualize.js
+++ js/visualize.js
@@ -1,19 +1,25 @@
 ---
 ---
-
+/* Load json files */
 let node_data = $.getJSON("{{site.baseurl}}/json/wcma-collection--color.json");
 let exhibit_data = $.getJSON("{{site.baseurl}}/json/exhibitions--refactored.json");
 let graph_data = $.getJSON("{{site.baseurl}}/json/collection_graph.json");
 
-$.when(node_data, exhibit_data, graph_data).then(function(){
+$.when(node_data, exhibit_data, graph_data).then(function(vnode, vex, vg){
   console.log("Data loaded.");
+  node_data = vnode[0];
+  exhibit_data = vex[0];
+  graph_data = vg[0];
   visualize();
 });
 
 function getNodeColors(node) {
-  let data = node_data[node.id]
+  let default_colors = ["#408fa7", "#408fa7", "#408fa7"];
+  let data = node_data[node.id];
   let colors = data.dominant_colors;
-  return colors.split("$")[0];
+  colors = colors || default_colors;
+  // console.log(data, colors);
+  return colors[0];
 }
 
 function visualize() {
@@ -23,14 +29,22 @@ function visualize() {
     .attr('width', width)
     .attr('height', height);
 
+  let nodes = graph_data["nodes"];
+
   const simulation = d3.forceSimulation()
     .force('charge', d3.forceManyBody().strength(-20))
     .force('center', d3.forceCenter(width / 2, height / 2));
 
   const nodeElements = svg.append('g')
     .selectAll('circle')
-    .data(graph_data.nodes)
+    .data(nodes)
     .enter().append('circle')
-    .attr('r', 10)
-    .attr('fill', getNodeColors)
+      .attr('r', 10)
+      .attr('fill', getNodeColors);
+
+  simulation.nodes(nodes).on('tick', () => {
+    nodeElements
+    .attr("cx", node => node.x)
+    .attr("cy", node => node.y)
+  });
 };
