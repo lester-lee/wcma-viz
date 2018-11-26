30923d56118d4e81244b0795bd504679e6e4f832
Lester Lee <ll6@williams.edu>
2018-11-26 18:01:35 -0500

    Nodes pannable and zoomable

--- js/visualize.js
+++ js/visualize.js
@@ -29,10 +29,25 @@ function visualize() {
     .attr('width', width)
     .attr('height', height);
 
-  let nodes = graph_data["nodes"];
+  svg.append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .style('fill', 'none')
+    .style('pointer-events', 'all')
+    .call(d3.zoom()
+      .scaleExtent([1/2, 4])
+      .on('zoom', zoomed));
+
+  function zoomed(){
+    nodeElements.attr('transform', d3.event.transform);
+    linkElements.attr('transform', d3.event.transform);
+  }
+
+  /* Show nodes */
+  let nodes = graph_data.nodes;
 
   const simulation = d3.forceSimulation()
-    .force('charge', d3.forceManyBody().strength(-20))
+    .force('charge', d3.forceManyBody().strength(-5))
     .force('center', d3.forceCenter(width / 2, height / 2));
 
   const nodeElements = svg.append('g')
@@ -42,9 +57,31 @@ function visualize() {
       .attr('r', 10)
       .attr('fill', getNodeColors);
 
+  /* Show links 
+  let links = graph_data.links;
+  simulation.force('link', d3.forceLink()
+    .id(link => link.id)
+    .strength(link => 0.7));
+
+  const linkElements = svg.append('g')
+    .selectAll('line')
+    .data(links)
+    .enter().append('line')
+      .attr('stroke-width', 1)
+      .attr('stroke', '#bbb');
+  */
   simulation.nodes(nodes).on('tick', () => {
     nodeElements
-    .attr("cx", node => node.x)
-    .attr("cy", node => node.y)
+      .attr('cx', node => node.x)
+      .attr('cy', node => node.y);
+
+    linkElements
+      .attr('x1', link => link.source.x)
+      .attr('y1', link => link.source.y)
+      .attr('x2', link => link.target.x)
+      .attr('y2', link => link.target.y);
   });
+
+  //simulation.force('link').links(links);
+
 };
